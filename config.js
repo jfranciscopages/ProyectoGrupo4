9001a0e9b224dc2bd56ec949d00752aae2a4d03a
Francisco <zextfpages@correoargentino.com.ar>
2024-11-13 19:50:03 -0300

    dotenv config

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
-const PORT = 8080
-const MODO_PERSISTENCIA = 'MONGODB' 
-const STRCNX = 'mongodb+srv://Ezequiel:Adonis2021@cluster0.g3sdd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-const BASE = 'baseProyectoG4'
-const accountSid = 'AC01823baf101eddf64d9a2a0f37ea15f4'
-const authToken = 'ac4d9747c867c66cffa05e48f56899e2'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+//console.log(process.env)
+
+const PORT = process.env.PORT || 8080
+const MODO_PERSISTENCIA = process.env.MODO_PERSISTENCIA 
+const STRCNX = process.env.STRCNX || 'mongodb://127.0.0.1'
+const BASE = process.env.BASE || 'test'
+const accountSid = process.env.accountSid || ''
+const authToken = process.env.authToken || ''
 
 export default {
     PORT,    
