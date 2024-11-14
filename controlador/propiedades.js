c5943c039e41447429cbd237a2736e57d0251973
Vanessa Tapia <tapiavanessa.vt@gmail.com>
2024-11-14 00:26:37 -0300

    Agregando Caso de Uso Excel

--- controlador/propiedades.js
+++ controlador/propiedades.js
@@ -43,6 +43,32 @@ class Controlador {
         const propiedadEliminada = await this.servicio.borrarPropiedad(id)
         res.json(propiedadEliminada)
     }
+
+    // Obtener el precio promedio por barrio
+    obtenerPrecioPromedio = async (req, res) => {
+        try {
+            const preciosPromedio = await this.servicio.obtenerPrecioPromedio();
+            res.json(preciosPromedio);  // Devolvemos los precios promedio como respuesta
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    }
+
+    // Generar y descargar el archivo Excel con los precios promedio por barrio
+    generarExcelPrecioPromedio = async (req, res) => {
+        try {
+            const excelBuffer = await this.servicio.generarExcelPrecioPromedio();
+
+            // Establecemos las cabeceras para indicar que es un archivo Excel
+            res.setHeader('Content-Disposition', 'attachment; filename=precios_promedio_barrio.xlsx');
+            res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+            
+            // Enviamos el buffer como archivo Excel
+            res.send(excelBuffer);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    }
 }
 
 export default Controlador
