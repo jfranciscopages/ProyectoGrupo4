fadee8b577e93a4744f68f2b7017f6dc37707ea9
Francisco <zextfpages@correoargentino.com.ar>
2024-11-20 20:49:36 -0300

    fix revert port

--- router/propiedades.js
+++ router/propiedades.js
@@ -9,13 +9,12 @@ class Router {
     start() {
         const router = express.Router()
 
-        router.get('/precio-promedio', this.controlador.obtenerPrecioPromedio); // Ruta para precios promedio
-        router.get('/precio-promedio/excel', this.controlador.generarExcelPrecioPromedio); // Ruta para generar y descargar el archivo Excel con los precios promedio
         router.get('/:id?', this.controlador.obtenerPropiedades)
         router.post('/', this.controlador.guardarPropiedad)
         router.put('/:id', this.controlador.actualizarPropiedad)
         router.delete('/:id', this.controlador.borrarPropiedad)
-
+        router.get('/precio-promedio', this.controlador.obtenerPrecioPromedio); // Ruta para precios promedio
+        router.get('/precio-promedio/excel', this.controlador.generarExcelPrecioPromedio); // Ruta para generar y descargar el archivo Excel con los precios promedio
 
         return router
     }
